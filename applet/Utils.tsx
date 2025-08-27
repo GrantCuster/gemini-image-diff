fd77ef1bac737990c6e10f293963fa5c89b78cde
Grant Custer <grantcuster@gmail.com>
2025-08-27 10:10:02 -0400

    another image fix

--- applet/Utils.tsx
+++ applet/Utils.tsx
@@ -1,7 +1,7 @@
 export function loadImage(src: string): Promise<HTMLImageElement> {
   return new Promise((resolve, reject) => {
     const img = new Image();
-    img.crossOrigin = "Anonymous"; // Handle CORS if needed
+    img.crossOrigin = "anonymous"; // Handle CORS if needed
     img.onload = () => resolve(img);
     img.onerror = reject;
     img.src = src;
